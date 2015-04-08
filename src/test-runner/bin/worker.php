c9cfca055a10dccab51f3930ddf8ea5ce59fa7aa
fram <fm.de.jouvencel@gmail.com>
2015-04-08 14:22:33 +0200

    // a notice IS an error

--- src/test-runner/bin/worker.php
+++ src/test-runner/bin/worker.php
@@ -1,5 +1,12 @@
 <?php
 
+@ini_set('display_errors', 'on');
+
+function exception_error_handler($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+set_error_handler("exception_error_handler");
+
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', '..', 'vendor', 'autoload.php']);
 
 $serverAddress = $argv[1];
